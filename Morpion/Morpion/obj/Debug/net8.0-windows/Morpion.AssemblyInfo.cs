89e31766e1e2bacd0a988ec71736aaca973749a7
VitoDeriu <vito.deriu@ynov.com>
2024-11-17 18:33:09 +0100

    Ajout morpion au ReadMe

--- Morpion/Morpion/obj/Debug/net8.0-windows/Morpion.AssemblyInfo.cs
+++ Morpion/Morpion/obj/Debug/net8.0-windows/Morpion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Morpion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907d4b0ea22a225f8fb636f894c2f09657de607e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a140a876efa66c4734419d85ba6d3d66e321862")]
 [assembly: System.Reflection.AssemblyProductAttribute("Morpion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Morpion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
